65ea7956f6f52582ec56405abeeb9c4f86b437cc
D3Ti <62066592+TheD3Ti@users.noreply.github.com>
2023-04-12 13:32:48 -0700

    Create mac.py

---
+++ mac.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+
+o = input("mac : ")
+r = requests.get("https://api.macaddress.io/v1?apiKey=<api>&output=json&search=" + o)
+lklk = json.loads(r.text)
+print("oui " + lklk["vendorDetails"]["oui"])
+print("companyName " + lklk["vendorDetails"]["companyName"])
+print("companyAddress " + lklk["vendorDetails"]["companyAddress"])
+print("countryCode " + lklk["vendorDetails"]["countryCode"])
+print("borderLeft " + lklk["blockDetails"]["borderLeft"])
+print("borderRight " + lklk["blockDetails"]["borderRight"])
+print("assignmentBlockSize " + lklk["blockDetails"]["assignmentBlockSize"])
+print("dateCreated " + lklk["blockDetails"]["dateCreated"])
+print("dateUpdated " + lklk["blockDetails"]["dateUpdated"])
+print("searchTerm " + lklk["macAddressDetails"]["searchTerm"])
+print("virtualMachine " + lklk["macAddressDetails"]["virtualMachine"])
+print("transmissionType " + lklk["macAddressDetails"]["transmissionType"])
+print("administrationType " + lklk["macAddressDetails"]["administrationType"])
+print("wiresharkNotes " + lklk["macAddressDetails"]["wiresharkNotes"])
+print("comment " + lklk["macAddressDetails"]["comment"])
